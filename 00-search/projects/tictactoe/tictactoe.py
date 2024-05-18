cce6eb874d9fda1b3be80513a9bee707b79f109d
Jakob L. Truong <jakobtruong@gmail.com>
2024-05-18 15:01:06 -0700

    Downloaded project distribution code

---
+++ 00-search/projects/tictactoe/tictactoe.py
@@ -0,0 +1,81 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+
+    X gets first move when given an initial state of a board
+    """
+    is_even = True
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] != EMPTY:
+                is_even != is_even
+
+    return X if is_even else O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    possible_actions = set()
+    for row in range(len(board)):
+        for col in range(len(board[0])):
+            if board[row][col] == EMPTY:
+                set.add((row, col))
+
+    return possible_actions
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    raise NotImplementedError
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    raise NotImplementedError
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    raise NotImplementedError
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    raise NotImplementedError
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    raise NotImplementedError
