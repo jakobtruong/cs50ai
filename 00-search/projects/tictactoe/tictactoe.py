17f83945287c7ef1d7637fec4a777fb8c7218d31
Jakob L. Truong <jakobtruong@gmail.com>
2024-05-21 13:03:45 -0700

    Completed terminal function

--- 00-search/projects/tictactoe/tictactoe.py
+++ 00-search/projects/tictactoe/tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -50,7 +51,11 @@ def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    result_board = copy.deepcopy(board)
+    player_turn = player(result_board)
+    result_board[action[0]][action[1]] = player_turn
+
+    return result_board
 
 
 def winner(board):
@@ -64,7 +69,28 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    spaces_filled = True
+    # Checks rows and also checks if all spaces are not empty
+    for row in range(len(board)):
+        if board[row][0] != EMPTY and board[row][0] == board[row][1] == board[row][2]:
+            return True
+        if board[row][0] == EMPTY or board[row][1] == EMPTY or board[row][2] == EMPTY:
+            spaces_filled = False
+
+    # Checks columns
+    for col in range(len(board[0])):
+        if board[0][col] != EMPTY and board[0][col] == board[1][col] == board[2][col]:
+            return True
+
+    # Checks major diagonal
+    if board[0][0] != EMPTY and board[0][0] == board[1][1] == board[2][2]:
+        return True
+
+    # Checks minor diagonal
+    if board[0][2] != EMPTY and board[0][2] == board[1][1] == board[2][0]:
+        return True
+
+    return spaces_filled
 
 
 def utility(board):
