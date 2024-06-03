25f5af5d9f86ae698d57dd5aa6802241d5b76db0
Jakob L. Truong <jakobtruong@gmail.com>
2024-06-03 16:38:05 -0700

    Added knowledge for puzzles 0-3

--- 01-knowledge/projects/knights/puzzle.py
+++ 01-knowledge/projects/knights/puzzle.py
@@ -12,21 +12,56 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    # Knowledge based on the structure
+    # Character cannot be both a knight and a knave and a character must be one or the other
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    # Knowledge based on what the character A said
+    # Implications gathered by character A saying they are both a knight and a knave
+    Implication(AKnight, And(AKnight, AKnave)), # Implication that whatever the knights says is the truth
+    Implication(AKnave, Not(And(AKnight, AKnave))) # Implication that whataver is knave says is a lie
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    # Knowledge based on the structure
+    # Character cannot be both a knight and a knave and a character must be one or the other
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    # Knowledge based on what the character A said
+    # Implications gathered from A saying that they are both knaves
+    Implication(AKnight, And(AKnave, BKnave)),
+    Implication(AKnave, Not(And(AKnave, BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    # Knowledge based on the structureCharacter cannot be both a knight and a knave
+    # Character cannot be both a knight and a knave and a character must be one or the other
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    # Knowledge based on what the character A said
+    # Implications gathered from A saying A and B are of the same kind
+    Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))), # Implication that whatever the knights says is the truth
+    Implication(AKnave, Not(Or(And(AKnight, BKnight), And(AKnave, BKnave)))), # Implication that whataver is knave says is a lie
+
+    # Knowledge based on what the character B said
+    # Implications gathered from B saying A and B are of different kinds
+    Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    Implication(BKnave, Not(Or(And(AKnight, BKnave), And(AKnave, BKnight))))
 )
 
 # Puzzle 3
@@ -35,7 +70,47 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    # Knowledge based on the structure
+    # Character cannot be both a knight and a knave and a character must be one or the other
+    Or(AKnight, AKnave),
+    Not(And(AKnight, AKnave)),
+
+    Or(BKnight, BKnave),
+    Not(And(BKnight, BKnave)),
+
+    Or(CKnight, CKnave),
+    Not(And(CKnight, CKnave)),
+
+    # Knowledge based on what the character A said
+    # Either A says "I am a knight" or "I am a knave" but we don't know which one
+    Or(
+        And(
+            Implication(AKnight, AKnight),
+            Implication(AKnave, Not(AKnight))
+        ),
+        And(
+            Implication(AKnight, AKnave),
+            Implication(AKnave, Not(AKnave))
+        )
+    ),
+
+    # Knowledge based on what the character B said
+    # Implication that if B is a knight (tells the truth), says that A said that "I am a knave"
+    Implication(BKnight, And(
+        Implication(AKnight, AKnave),
+        Implication(AKnave, Not(AKnave)
+    ))),
+
+    # If character B is a knave, then B saying A said something is not true therefore no implications can be gathered in this case
+
+    # Implications gathered from character B saying C is a knave
+    Implication(BKnight, CKnave),
+    Implication(BKnave, Not(CKnave)),
+
+    # Knowledge based on what the character C said
+    # Implications gathered from character C saying A is a knight
+    Implication(CKnight, AKnight),
+    Implication(CKnave, Not(AKnight))
 )
 
 
