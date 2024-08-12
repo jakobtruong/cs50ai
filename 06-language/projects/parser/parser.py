2ce10db9c47b5650d08127472a864bd562881be0
Jakob L. Truong <jakobtruong@gmail.com>
2024-08-11 19:10:09 -0700

    Implemented preprocess function, np_chunk function, and non-terminals to pass tests

--- 06-language/projects/parser/parser.py
+++ 06-language/projects/parser/parser.py
@@ -15,7 +15,13 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> N V
+S -> NP VP | NP VP Conj VP | NP VP Conj NP VP
+
+NP -> N | Det NP | AdjP NP | NP PP
+VP -> V | V NP | AdvP VP | AdvP VP | V PP
+AdjP -> Adj | Adj Adj
+AdvP -> Adv
+PP -> P NP
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
@@ -62,7 +68,13 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    # Tokenizes each element in sentence
+    tokenized_words = nltk.tokenize.word_tokenize(sentence)
+
+    # Creates list lowercase words from tokenized elements that only have alphabet characters in them
+    processed_words = [word.lower() for word in tokenized_words if word.isalpha()]
+
+    return processed_words
 
 
 def np_chunk(tree):
@@ -72,7 +84,23 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    # Returns True value if "NP" exists within any child subtrees of current subtree
+    def contains_NP(subtree):
+        for child_subtree in subtree.subtrees():
+            if child_subtree == subtree:
+                continue
+            elif child_subtree.label() == "NP":
+                return True
+        return False
+
+    result = []
+
+    # Iterates through subtrees that have a label of "NP"
+    for subtree in tree.subtrees(lambda x: x.label() == "NP"):
+        # Checks if subtrees children contains more "NP"
+        if not contains_NP(subtree):
+            result.append(subtree)
+    return result
 
 
 if __name__ == "__main__":
